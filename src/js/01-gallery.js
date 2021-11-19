a32af9d83a8a0e69cb9ae4c041d8a59b6b06e7f3
KseniaLF <llf9391@gmail.com>
2021-11-19 17:31:07 +0200

    галерея

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+addMarcup(galleryItems);
+
+function addMarcup(arr) {
+    const murcup = arr.map(imgEl => ` 
+    <a class="gallery__item" href="${imgEl.original}"
+    title="${imgEl.description}">
+        <img class="gallery__image" src="${imgEl.preview}"
+        alt="${imgEl.description}" />
+    </a>
+    `).join('');
+    galleryContainer.innerHTML = murcup;
+}
+
+galleryContainer.addEventListener("click", fnClick)
+
+function fnClick(event) {
+    event.preventDefault();
+}
+
+var lightbox = new SimpleLightbox('.gallery a',
+    {captionsData:'alt', captionDelay:250
+ });
+// 
